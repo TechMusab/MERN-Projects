f391a917c349503909f878354f288df56a01d83e
Musab <joiya4636@gmail.com>
2025-06-25 14:38:08 -0700

    Frontend done

--- Day1-ToDo-App/client/src/App.jsx
+++ Day1-ToDo-App/client/src/App.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
 import Tasks from "./components/Tasks";
 function App() {
+  const[input,setinput]=useState("")
+  const[tasks,settask]=useState([]);
+  const[edit,setedit]=useState(null)
+
+  const addtask = () => {
+    if (input.trim() === "") {
+      alert("Please enter a task");
+      return;
+    }
+  
+    if (edit !== null) {
+      const updatedTasks = tasks.map((task) =>
+        task.id === edit ? { ...task, description: input } : task
+      );
+      settask(updatedTasks);
+      setedit(null); 
+    } else {
+      const newtask = {
+        id: Date.now(),
+        description: input,
+      };
+      settask([...tasks, newtask]);
+    }
+    setinput(""); 
+  };
+
+
+  const onDelete=(id)=>{
+    const updatedTasks=tasks.filter((task)=> task.id!=id)
+    settask(updatedTasks);
+  }
+
+  
   return (
     <>
       <h1 className="text-center text-bold bg-green-700 text-white text-2xl p-6 ">
@@ -7,15 +41,17 @@ function App() {
       </h1>
       <div className="w-full flex justify-center items-center gap-6 my-8">
         <input
+        value={input}
+        onChange={(e)=>setinput(e.target.value)}
           type="text"
           className="p-4 w-1/2 border border-green-900 outline-none text-black"
           placeholder="Enter Task"
         />
-        <button className="p-4 bg-green-600  rounded-2xl text-white hover:bg-green-800 cursor-pointer  ">
+        <button onClick={addtask} className="p-4 bg-green-600  rounded-2xl text-white hover:bg-green-800 cursor-pointer  ">
           Add Task
         </button>
       </div>
-        <Tasks />
+        <Tasks tasklist={tasks} onDelete={onDelete} setedit={setedit} setinput={setinput}  />
     </>
   );
 }
--- Day1-ToDo-App/client/src/components/Tasks.jsx
+++ Day1-ToDo-App/client/src/components/Tasks.jsx
@@ -1,40 +1,24 @@
 import React from "react";
 import { MdDeleteOutline } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
-export default function Tasks() {
-  const Tasks = [
-    {
-      id: 1,
-      title: "Task 1",
-      description: "This is the first task",
-      completed: false,
-    },
-    {
-      id: 2,
-      title: "Task 2",
-      description: "This is the second task",
-      completed: true,
-    },
-    {
-      id: 3,
-      title: "Task 3",
-      description: "This is the third task",
-      completed: false,
-    },
-  ];
+export default function Tasks({tasklist,onDelete,setedit,setinput}) {
+
 
   return (
     <>
       <div className="bg-green-500 w-1/2 m-auto h-auto flex flex-col justify-center items-center rounded-2xl p-4">
         <h1 className="text-4xl text-white text-bold font-bold">Tasks</h1>
         <ul className="my-4">
-          {Tasks.map((task) => {
+          {tasklist.map((task) => {
             return (
               <li className="flex min-w-[500px] w-fit bg-gray-300 rounded-2x p-6 mt-4 rounded-lg justify-between items-center text-black">
                 <p className=" text-2xl">{task.description}</p>
                 <div className="flex gap-2">
-                <FaEdit className="cursor-pointer" size={28} color="black" />
-                  <MdDeleteOutline className="cursor-pointer" size={28} color="red" />
+                <FaEdit onClick={()=>{
+                    setedit(task.id);
+                    setinput(task.description); 
+                }} className="cursor-pointer" size={28} color="black" />
+                  <MdDeleteOutline onClick={()=>onDelete(task.id)} className="cursor-pointer" size={28} color="red" />
                  
                 </div>
               </li>
